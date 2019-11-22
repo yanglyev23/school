00eb190247de5bfe8ebcc28f801d0a02c0bc6f42
yanglyev23 <yanglyev23@icloud.com>
2019-11-22 16:06:49 +0300

    Add files via upload

---
+++ school/school/school/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  school
+//
+//  Created by Admin on 20.11.2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
